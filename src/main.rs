058cac83760541ace195addb863b2f4f64d2afba
Tokarev Artem <tokarev28.art@gmail.com>
2021-12-01 23:18:20 +0500

    rotator and backup for ori opt

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ use std::time::Instant;
 
 type UnitQuat = UnitQuaternion<f64>;
 use fnd::FundAngles;
+
+use crate::ori_opt::{OptResult, Rotator};
 #[derive(Clone, Copy, Debug)]
 pub struct GrainOrientation {
     pub quat: UnitQuat, 
@@ -854,70 +856,70 @@ mod mis_opt {
 mod ori_opt {
     use crate::*;
 
-    pub fn texture_sum(grid: &mut fnd::FundGrid) -> f64 {
+    pub fn texture_sum(grid: &fnd::FundGrid) -> f64 {
         grid.cells.iter().flatten().flatten()
             .map(|&x| x * x)
             .sum()
     }
 
-    pub fn texture_index(grid: &mut fnd::FundGrid) -> f64 {
+    pub fn texture_index(grid: &fnd::FundGrid) -> f64 {
         texture_sum(grid) * grid.dvol
     }
 
     // returns previus and current orientation cell idxs and cell heights
-    fn rotate_randomly(
-        g: &mut PolyGraph, grid: &mut fnd::FundGrid, 
-        n: NodeIndex, texture_sum: &mut f64, rng: &mut impl Rng
-    ) -> ((fnd::CellIdxs, f64), (fnd::CellIdxs, f64)) {
-        let vol = g[n].volume;
+    // fn rotate_randomly(
+    //     g: &mut PolyGraph, grid: &mut fnd::FundGrid, 
+    //     n: NodeIndex, texture_sum: &mut f64, rng: &mut impl Rng
+    // ) -> ((fnd::CellIdxs, f64), (fnd::CellIdxs, f64)) {
+    //     let vol = g[n].volume;
 
-        let (prev_idxs, prev_h1) = {
-            let idxs = grid.idxs(g[n].orientation.fund);
-            let prev_h = grid.at(idxs);
-            *grid.at_mut(idxs) -= vol;
-            (idxs, prev_h)
-        };
-        let (cur_idxs, prev_h2) = {
-            g[n].orientation = GrainOrientation::random(rng);
-            let mut idxs = grid.idxs(g[n].orientation.fund);
-            while idxs == prev_idxs {
-                g[n].orientation = GrainOrientation::random(rng);
-                idxs = grid.idxs(g[n].orientation.fund);
-            }
-            let prev_h = grid.at(idxs);
-            *grid.at_mut(idxs) += vol;
-            (idxs, prev_h)
-        };
-        *texture_sum += 2.0 * vol * ((prev_h2 - prev_h1) + vol);
-        ((prev_idxs, prev_h1), (cur_idxs, prev_h2))
-    }
+    //     let (prev_idxs, prev_h1) = {
+    //         let idxs = grid.idxs(g[n].orientation.fund);
+    //         let prev_h = grid.at(idxs);
+    //         *grid.at_mut(idxs) -= vol;
+    //         (idxs, prev_h)
+    //     };
+    //     let (cur_idxs, prev_h2) = {
+    //         g[n].orientation = GrainOrientation::random(rng);
+    //         let mut idxs = grid.idxs(g[n].orientation.fund);
+    //         while idxs == prev_idxs {
+    //             g[n].orientation = GrainOrientation::random(rng);
+    //             idxs = grid.idxs(g[n].orientation.fund);
+    //         }
+    //         let prev_h = grid.at(idxs);
+    //         *grid.at_mut(idxs) += vol;
+    //         (idxs, prev_h)
+    //     };
+    //     *texture_sum += 2.0 * vol * ((prev_h2 - prev_h1) + vol);
+    //     ((prev_idxs, prev_h1), (cur_idxs, prev_h2))
+    // }
     
-    pub fn iterate_rotations_cubic_isotropic(
-        g: &mut PolyGraph, grid: &mut fnd::FundGrid, 
-        texture_sum: &mut f64, rng: &mut impl Rng,
-    ) -> Option<f64> {
+    // pub fn iterate_rotations_cubic_isotropic(
+    //     g: &mut PolyGraph, grid: &mut fnd::FundGrid, 
+    //     texture_sum: &mut f64, rng: &mut impl Rng,
+    // ) -> Option<f64> {
 
-        let distr = RandUniform::new(0, g.node_count() as u32);
-        let n: NodeIndex = rng.sample(distr).into();
+    //     let distr = RandUniform::new(0, g.node_count() as u32);
+    //     let n: NodeIndex = rng.sample(distr).into();
         
-        let prev_ori = g[n].orientation;
-        let prev_texsum = *texture_sum;
-        let (prev, cur) = rotate_randomly(g, grid, n, texture_sum, rng);
-        if *texture_sum < prev_texsum {
-            Some(*texture_sum * grid.dvol)
-        } else {
-            g[n].orientation = prev_ori;
-            // restoration order matters in case 
-            // the new orientation is in the same cell as the previous one
-            *grid.at_mut(cur.0) = cur.1;
-            *grid.at_mut(prev.0) = prev.1;
-            *texture_sum = prev_texsum;
-            None
-        }
-    }
+    //     let prev_ori = g[n].orientation;
+    //     let prev_texsum = *texture_sum;
+    //     let (prev, cur) = rotate_randomly(g, grid, n, texture_sum, rng);
+    //     if *texture_sum < prev_texsum {
+    //         Some(*texture_sum * grid.dvol)
+    //     } else {
+    //         g[n].orientation = prev_ori;
+    //         // restoration order matters in case 
+    //         // the new orientation is in the same cell as the previous one
+    //         *grid.at_mut(cur.0) = cur.1;
+    //         *grid.at_mut(prev.0) = prev.1;
+    //         *texture_sum = prev_texsum;
+    //         None
+    //     }
+    // }
 
     #[derive(Debug, Clone, Copy)]
-    struct PrevData {
+    struct CellBackup {
         idxs: fnd::CellIdxs,
         height: f64,
     }
@@ -926,23 +928,104 @@ mod ori_opt {
     pub struct RotatorBackup {
         grain_idx: NodeIndex, 
         texture_sum: f64,
-        prev_ori_data: PrevData,
-        cur_ori_data: PrevData,
+        prev_ori: GrainOrientation,
+        prev_cell_bu: CellBackup,
+        cur_cell_bu: CellBackup,
+    }
+
+    pub enum OptResult {
+        MoreOptimal(f64),
+        SameOrLessOptimal(f64),
     }
 
     #[derive(Debug, Clone)]
     pub struct Rotator {
         backup: Option<RotatorBackup>,
+        texture_sum: f64,
     }
 
     impl Rotator {
+        pub fn new(grid: &fnd::FundGrid) -> Self {
+            Self{ backup: None, texture_sum: texture_sum(grid) }
+        }
 
+        pub fn texture_index(&self, grid: &fnd::FundGrid) -> f64 {
+            self.texture_sum * grid.dvol
+        }
+
+        pub fn rotate_grain_ori(
+            &mut self, grain_idx: NodeIndex, g: &mut PolyGraph, 
+            grid: &mut fnd::FundGrid, rng: &mut impl Rng,
+        ) -> OptResult {
+            
+            let vol = g[grain_idx].volume;
+            let prev_texsum = self.texture_sum;
+
+            let prev_ori = g[grain_idx].orientation;
+            let prev_idxs = grid.idxs(prev_ori.fund);
+            let prev_h1 = grid.at(prev_idxs);
+            *grid.at_mut(prev_idxs) -= vol;
+            let prev_cell_bu = CellBackup{ idxs: prev_idxs, height: prev_h1 };
+
+            let mut cur_ori = GrainOrientation::random(rng);
+            let mut cur_idxs = grid.idxs(cur_ori.fund);
+            while cur_idxs == prev_idxs {
+                cur_ori = GrainOrientation::random(rng);
+                cur_idxs = grid.idxs(cur_ori.fund);
+            }
+            g[grain_idx].orientation = cur_ori;
+            let prev_h2 = grid.at(cur_idxs);
+            *grid.at_mut(cur_idxs) += vol;
+            let cur_cell_bu = CellBackup{ idxs: cur_idxs, height: prev_h2 };
+
+            self.texture_sum += 2.0 * vol * ((prev_h2 - prev_h1) + vol);
+            
+            let backup = RotatorBackup{ 
+                grain_idx, 
+                texture_sum: prev_texsum,
+                prev_ori, 
+                prev_cell_bu, 
+                cur_cell_bu,
+            };
+            self.backup = Some(backup);
+
+            let texidx = self.texture_sum * grid.dvol;
+            if self.texture_sum < prev_texsum {
+                OptResult::MoreOptimal(texidx)
+            } else {
+                OptResult::SameOrLessOptimal(texidx)
+            }
+        }
+
+        pub fn rotate_random_grain_ori(
+            &mut self, g: &mut PolyGraph, grid: &mut fnd::FundGrid, 
+            rng: &mut impl Rng,
+        ) -> OptResult {
+
+            let distr = RandUniform::new(0, g.node_count() as u32);
+            let grain_idx: NodeIndex = rng.sample(distr).into();
+            self.rotate_grain_ori(grain_idx, g, grid, rng)
+        }
+
+        pub fn restore_previous(&mut self, g: &mut PolyGraph, grid: &mut fnd::FundGrid) {
+            let RotatorBackup{ 
+                grain_idx, 
+                prev_ori, 
+                prev_cell_bu, 
+                cur_cell_bu, 
+                texture_sum 
+            } = self.backup.take().unwrap();
+
+            g[grain_idx].orientation = prev_ori;
+            // restoration order matters in case 
+            // the new orientation is in the same cell as the previous one
+            *grid.at_mut(cur_cell_bu.idxs) = cur_cell_bu.height;
+            *grid.at_mut(prev_cell_bu.idxs) = prev_cell_bu.height;
+            self.texture_sum = texture_sum;
+        }
     }
 
-    // pub fn rotate_cubic_isotropic(
-    //     g: &mut PolyGraph, grid: &mut fnd::FundGrid, 
-    //     texture_sum: &mut f64, rng: &mut impl Rng,
-    // )
+    
 }
 
 fn rotate_to_fund_domain(o: UnitQuat, syms: &Vec<UnitQuat>) -> UnitQuat {
@@ -1099,18 +1182,20 @@ fn main() {
     println!("min max f: {:?}", minmax(&grid));
     
     let now = Instant::now();
-    let mut texture_sum = ori_opt::texture_sum(&mut grid);
-    println!("starting texture index: {}", texture_sum * grid.dvol);
+    let mut rotator = ori_opt::Rotator::new(&grid);
+    println!("starting texture index: {}", rotator.texture_index(&grid));
     for i in 0..10_000_000 {
-        if let Some(texidx) = ori_opt::iterate_rotations_cubic_isotropic(
-            &mut g, &mut grid, &mut texture_sum, &mut rng
+        if let OptResult::MoreOptimal(texidx) = rotator.rotate_random_grain_ori(
+            &mut g, &mut grid, &mut rng
         ) {
             // println!("iter {}, texture index {}", i, texidx);
+        } else {
+            rotator.restore_previous(&mut g, &mut grid);
         }
     }
     println!(
         "rotations alg time: {} s, texture index {}", 
-        now.elapsed().as_secs_f64(), texture_sum * grid.dvol
+        now.elapsed().as_secs_f64(), rotator.texture_index(&grid)
     );
 
     println!("min max f: {:?}", minmax(&grid));
